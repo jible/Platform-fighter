e04b8b2f2bda402096f10e780c25fc55454f4c09
jible <jamezman88th@gmail.com>
2025-08-01 13:55:49 -0700

    updated animation manager
    animation manager no longer child of character body.
    
    animation manager now snaps to pixels, following the character body. Not currently in a suitable state. Needs testing a brainstorming

--- platform-fighter/prefabs/components/player/animation_manager.gd
+++ platform-fighter/prefabs/components/player/animation_manager.gd
@@ -2,9 +2,8 @@ class_name AnimationManager
 extends AnimatedSprite2D
 
 # Based on https://github.com/jible/capstone/blob/main/scripts/characters/animation_manager.gd
-
+@export var player_body: CharacterBody2D
 var current_state: String = ""
-
 func update_anim():
 	play(current_state)
 	return
@@ -13,3 +12,9 @@ func update_anim():
 func _on_state_machine_state_changed(new_state_node):
 	current_state = new_state_node.name
 	update_anim()
+
+
+
+func _process(delta):
+	print(scale)
+	position = scale.y * (player_body.position/scale.y).floor()
