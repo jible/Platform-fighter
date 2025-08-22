f17ed08e6002fe045ab7b126c9830699c202d8c1
jible <jamezman88th@gmail.com>
2025-08-22 13:33:50 -0700

    fixed animation updater
    There were more inbetween classes that I accounted for.
    The AnimationPlayer extends AnimationMixer. Which stores the animations. It stores those animations in AnimationLibraries. I did not account for all of these data types before.
    
    Had to push this before testing the script.

--- platform-fighter/components/for_characters/generalized/animation_updater.gd
+++ platform-fighter/components/for_characters/generalized/animation_updater.gd
@@ -11,25 +11,36 @@ extends Node
 # References
 @export var sprite_manager: SpriteManager
 @export var animation_player: AnimationPlayer
+var anim_library_name: String = ""
+var anim_library: AnimationLibrary = null
+
+func get_standard_anim_library():
+	anim_library = animation_player.get_animation_library(anim_library_name)
+	if !anim_library:
+		anim_library = AnimationLibrary.new()
+	animation_player.add_animation_library(anim_library_name, anim_library)
 
 
 func update_player(): 
+	if !anim_library:
+		get_standard_anim_library()
+	
 	var anim_names = Array(sprite_manager.sprite_frames.get_animation_names())
 	for anim_name in anim_names:
 		# Extract anim info from sprite manager
-		var speed = sprite_manager.get_animation_speed(anim_name)
-		var frame_count =  sprite_manager.get_frame_count(anim_name)
+		var speed = sprite_manager.sprite_frames.get_animation_speed(anim_name)
+		var frame_count =  sprite_manager.sprite_frames.get_frame_count(anim_name)
 		var loop = sprite_manager.sprite_frames.get_animation_loop(anim_name)
 		# Add animation or update animation
 		var anim = animation_player.get_animation(anim_name)
 		if !anim:
 			anim = Animation.new()
-			animation_player.add_animation(anim_name, anim)
+			anim_library.add_animation(anim_name, anim)
 		if loop:
 			anim.loop_mode = Animation.LOOP_LINEAR
 		else:
-			anim.loop_mode = Animation.LOOP_LINEAR
-		anim.length =  frame_count/speed
+			anim.loop_mode = Animation.LOOP_NONE
+		anim.length = frame_count/speed
 		
 		# TODO: Maybe remove all other anim tracks!
 		
@@ -37,7 +48,7 @@ func update_player():
 		anim.track_set_path(track, "%s:frame" %sprite_manager.name)
 		
 		for key in range(frame_count):
-			anim.track_insert_key(track,key * speed, key)
+			anim.track_insert_key(track,float(key) / float(speed), key)
 			pass
 		continue
 		
