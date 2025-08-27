4771ef360d54053171c54a2536eecab6b59adc10
jible <jamezman88th@gmail.com>
2025-08-27 11:55:15 -0700

    Mobility manager and character state,
    Changed character state to derive from Node2D instead of Node. Now, states can parent objects that pertain to their state and they will follow the character body. Before, attached hitboxes would not follow the player.
    
    Added mobility manager functions including: "add_capped_velocity_impulse", "add_uncapped_velocity_impulse", "set_capped_velocity", and set_uncapped_velocity". These functions are made to prevent other nodes from directly modifying the velocity of the character body. Once tested, the functions should also be implemented into other mobility manager process functions.

--- platform-fighter/components/for_characters/generalized/physics_related/mobility_manager.gd
+++ platform-fighter/components/for_characters/generalized/physics_related/mobility_manager.gd
@@ -35,11 +35,13 @@ var current_state_type: state_types = state_types.NO_PROCESS
 @export var grounded_drag = 20
 @export var aeriel_drag = 2
 @export var max_horizontal_velocity = 500
-@export var max_fall_velocity = 800
+@export var max_vertical_velocity = 800
 @export var velocity_threshold = .5
 
-var max_ariel_jumps = 2
+@export var max_ariel_jumps = 2
 var used_ariel_jumps = 0
+# converts max_velocities to a vector so later functions can itterate through axis.
+@onready var max_velocity_vector = Vector2(max_horizontal_velocity, max_vertical_velocity)
 
 func _physics_process(delta):
 	# State Decides the movement processes that occur each frame
@@ -74,8 +76,8 @@ func standard_gravity_process(delta, _input_dir):
 		character_body.velocity.y = 0
 		return
 	character_body.velocity.y += gravity_force * delta
-	if character_body.velocity.y > max_fall_velocity:
-		character_body.velocity.y = max_fall_velocity
+	if character_body.velocity.y > max_vertical_velocity:
+		character_body.velocity.y = max_vertical_velocity
 
 func can_jump() ->bool:
 	return character_body.is_on_floor() or used_ariel_jumps < max_ariel_jumps
@@ -92,3 +94,35 @@ func _on_base_player_landed():
 
 func _on_state_machine_state_changed(new_state_node):
 	current_state_type = new_state_node.state_type
+
+func add_capped_velocity_impulse(impulse_vector):
+	# For each axis
+	for axis in ["x", "y"]:
+		if impulse_vector[axis] == 0: continue
+		var added_x_velocities = character_body.velocity[axis] + impulse_vector[axis]
+		# If velociy is added in the same direction:
+		if sign(impulse_vector[axis]) == sign(character_body.velocity[axis]):
+			# If starting velocity is within max velocity range:
+			if abs(character_body.velocity[axis]) < max_velocity_vector:
+				# Add velocity and cap it
+				character_body.velocity[axis] = added_x_velocities
+				character_body.velocity[axis] = clamp(character_body.velocity[axis], -max_velocity_vector, max_velocity_vector)
+			# Don't add velocity if it already exceeds max
+		else:
+			# If adding the velocity results in same velocity direction 
+			# (despite adding magintude in another direction), add it.
+			if sign(added_x_velocities) == sign(character_body[axis]):
+				character_body.velocity[axis] = added_x_velocities
+			# If adding the velocity changes the direction, add it and clamp it.
+			else:
+				character_body.velocity[axis] = clamp(character_body.velocity[axis], -max_horizontal_velocity, max_horizontal_velocity)
+
+func add_uncapped_velocity(velocity_impulse):
+	character_body.velocity += velocity_impulse
+
+func set_capped_velocity(new_velocity):
+	for axis in ['x','y']:
+		character_body.velocity[axis] = clamp(new_velocity[axis],-max_velocity_vector[axis],max_velocity_vector[axis])
+
+func set_uncapped_velocity(new_velocity):
+	character_body.velocity = new_velocity
--- platform-fighter/components/for_characters/generalized/state_helpers/character_state.gd
+++ platform-fighter/components/for_characters/generalized/state_helpers/character_state.gd
@@ -1,5 +1,5 @@
 class_name CharacterState
-extends Node
+extends Node2D
 
 enum LockLevel {
 	FREE,
