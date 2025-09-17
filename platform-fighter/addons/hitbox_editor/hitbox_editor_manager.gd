4babc6a8840f48eaa2ca30e70b79b677edd26d65
jible <jamezman88th@gmail.com>
2025-09-16 17:40:35 -0700

    Hitbox editor update
    entering a character body scene now collects references to state sand hitboxes belonging to selected state.

--- platform-fighter/addons/hitbox_editor/hitbox_editor_manager.gd
+++ platform-fighter/addons/hitbox_editor/hitbox_editor_manager.gd
@@ -6,16 +6,31 @@ extends VBoxContainer
 '''
 On start, this loads the characters from the character folder
 '''
-
+var states = []
+var character_root
+var state_machine
+var current_state_hitboxes = []
 func _ready():
 	pass
 
-func configure(character_root: BaseCharacter):
+func configure(_character_root: BaseCharacter):
+	character_root = _character_root
 	character_name_label.text = character_root.name
+	state_machine = character_root.state_machine
+	states = character_root.state_machine.get_children(true)
 	
-	var states = []
-	character_root.state_machine.states.keys()
 	state_drop_down.clear()
 	for state in states:
 		state_drop_down.add_item(state.name)
-	pass
+	_on_option_button_item_selected(state_drop_down.selected)
+
+
+
+func _on_option_button_item_selected(index):
+	var current_state_name = state_drop_down.get_item_text(index)
+	var state = state_machine.find_child(current_state_name)
+	if !state:
+		print("state does not exist")
+		return
+	current_state_hitboxes = state.get_children()
+	print(current_state_hitboxes)
