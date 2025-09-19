4a2fb00abc6a67717559acd0cd07d238ee7b4d9d
jible <jamezman88th@gmail.com>
2025-09-19 13:41:52 -0700

    correctly extracting info from animations

--- platform-fighter/addons/hitbox_editor/hitbox_editor_manager.gd
+++ platform-fighter/addons/hitbox_editor/hitbox_editor_manager.gd
@@ -9,6 +9,7 @@ extends VBoxContainer
 On start, this loads the characters from the character folder
 '''
 var states = []
+var state 
 var character_root
 var state_machine
 var current_state_hitboxes = []
@@ -32,21 +33,29 @@ func configure(_character_root: BaseCharacter):
 
 func _on_state_drop_down_item_selected(index):
 	var current_state_name = state_drop_down.get_item_text(index)
-	var state = state_machine.find_child(current_state_name)
+	state = state_machine.find_child(current_state_name)
 	if !state:
 		print("state does not exist")
 		return
 	update_hitboxes(state.get_children())
 
 func update_hitboxes(hitboxes):
+	hitbox_drop_down.clear()
 	current_state_hitboxes = hitboxes
 	for hitbox in current_state_hitboxes:
 		hitbox_drop_down.add_item(hitbox.name)
-	if current_state_hitboxes.size() > 0:
+	if current_state_hitboxes.size() > 0: # dont select if there is no hitbox
 		_on_hitbox_drop_down_item_selected(hitbox_drop_down.selected)
 
 func _on_hitbox_drop_down_item_selected(index):
-	var new_hitbox = find_child(hitbox_drop_down.get_item_text(index))
+	var new_hitbox = get_hitbox_from_name(hitbox_drop_down.get_item_text(index))
 	if new_hitbox:
-		hitbox_selection.update_hitbox(new_hitbox, character_root,state_machine)
-	
+		hitbox_selection.update_hitbox(new_hitbox, character_root, state)
+	else:
+		push_error("Could not find hitbox")
+
+func get_hitbox_from_name(hitbox_name):
+	for hitbox in current_state_hitboxes:
+		if hitbox.name == hitbox_name:
+			return hitbox
+	return null
--- platform-fighter/addons/hitbox_editor/ui_helpers/hitbox_selection.gd
+++ platform-fighter/addons/hitbox_editor/ui_helpers/hitbox_selection.gd
@@ -1,7 +1,10 @@
+@tool
 extends VBoxContainer
 
 
 @export var interpolate: CheckBox
+@export var starting_pos_x: SpinBox
+@export var starting_pos_y: SpinBox
 @export var offset_info: VBoxContainer
 @export var turn_on_frames: SpinBox
 @export var turn_off_frames: SpinBox
@@ -13,23 +16,26 @@ extends VBoxContainer
 var hitbox:Hitbox
 var base_character
 
-func update_hitbox(_hitbox, _base_character, state_name):
+func update_hitbox(_hitbox, _base_character, state):
 	hitbox = _hitbox
 	base_character = _base_character
-	var animation_player = base_character.animation_player
+	var animation_player: AnimationPlayer = base_character.animation_player
 	var animation_library = animation_player.get_animation_library("")
-	var animation: Animation = animation_library.get_animation(state_name)
+	
+	var animation: Animation = animation_library.get_animation(state.name)
 	
 	var pos_track
 	var func_track
 	var hitbox_position_track
-	var hitbox_path = get_node_path_from_root(hitbox)
-	for track in animation.get_track_count():
+	var hitbox_path = animation_player.owner.get_path_to(hitbox)
+	for track in range(animation.get_track_count()):
 		var track_type = animation.track_get_type(track)
 		if track_type == Animation.TrackType.TYPE_VALUE:
 			var interpolated: bool = animation.track_get_interpolation_type(track)
 			var path = animation.track_get_path(track)
-			if path != hitbox_path + ":position": continue
+			print(path)
+			if path.get_concatenated_names() != hitbox_path.get_concatenated_names() \
+			and path.get_concatenated_subnames() != "position": continue
 			for key in animation.track_get_key_count(track):
 				var time = animation.track_get_key_time(track, key)
 				var position = animation.track_get_key_value(track, key)
@@ -37,12 +43,13 @@ func update_hitbox(_hitbox, _base_character, state_name):
 				# add keys to ui
 		elif track_type == Animation.TrackType.TYPE_METHOD:
 			var path = animation.track_get_path(track)
-			if animation.track_get_path(track) != path: continue
+			if path.get_concatenated_names() != hitbox_path.get_concatenated_names(): continue
 			for key in animation.track_get_key_count(track):
 				if animation.method_track_get_name(track,key) == "turn_on":
 					print("turn on", animation.track_get_key_time(track, key))
 					# Add keys to ui
-		
+				elif animation.method_track_get_name(track,key) == "turn_off":
+					print("turn off", animation.track_get_key_time(track, key))
 	'''
 	Update ui
 	
@@ -56,17 +63,3 @@ func update_hitbox(_hitbox, _base_character, state_name):
 	and 
 	'''
 	
-	
-	
-func get_node_path_from_root(node):
-	'''
-	Walk up the tree until you hit the root
-	'''
-	var path = ""
-	var root = node.owner
-	var walk_node = node
-	while node!= root:
-		walk_node = walk_node.get_parent()
-		path =  walk_node.name+ path
-		if node!= root:
-			path = "/" + path
