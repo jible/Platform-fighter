c1ed43473853319ac458a9ba5864e9d39928bbd9
jible <jamezman88th@gmail.com>
2025-09-24 12:44:09 -0700

    testing adding and removing hitboxes

--- platform-fighter/addons/hitbox_editor/hitbox_editor_manager.gd
+++ platform-fighter/addons/hitbox_editor/hitbox_editor_manager.gd
@@ -10,26 +10,21 @@ extends VBoxContainer
 @export var state_drop_down: OptionButton
 @export var hitbox_drop_down: OptionButton
 @export var hitbox_selection: VBoxContainer
-'''
-On start, this loads the characters from the character folder
-'''
+
 var states = []
-var state 
+var state :CharacterState
 var character_root
 var state_machine
 var animation_player: AnimationPlayer
 var current_state_hitboxes = []
 
-func _ready():
-	pass
 
 func _process(delta):
 	if Engine.is_editor_hint() and character_root != null:
 		frame_slider.set_block_signals(true)
 		frame_slider.value = animation_player.current_animation_position
 		frame_slider.set_block_signals(false)
-		
-	pass
+
 
 func configure(_character_root: BaseCharacter):
 	character_root = _character_root
@@ -43,6 +38,55 @@ func configure(_character_root: BaseCharacter):
 		state_drop_down.add_item(state.name)
 	_on_state_drop_down_item_selected(state_drop_down.selected)
 
+# 
+func update_hitboxes(hitboxes):
+	hitbox_drop_down.clear()
+	current_state_hitboxes = hitboxes
+	for hitbox in current_state_hitboxes:
+		hitbox_drop_down.add_item(hitbox.name)
+	if current_state_hitboxes.size() > 0: # dont select if there is no hitbox
+		_on_hitbox_drop_down_item_selected(hitbox_drop_down.selected)
+		hitbox_selection.show()
+	else:
+		hitbox_selection.hide()
+
+func get_hitbox_from_name(hitbox_name):
+	for hitbox in current_state_hitboxes:
+		if hitbox.name == hitbox_name:
+			return hitbox
+	return null
+
+func add_hitbox():	
+	var new_hitbox = Area2D.new()
+	new_hitbox.set_script(load(hitbox_script_path))
+	state.add_child(new_hitbox)
+	
+	var collision = CollisionShape2D.new()
+	collision.shape = CircleShape2D.new()
+	collision.shape.radius = default_hitbox_radius
+	new_hitbox.add_child(collision)
+	
+	# Add new hitbox to drop down and select it
+	hitbox_drop_down.add_item(new_hitbox.name)
+	hitbox_drop_down.select(-1)
+	
+	hitbox_drop_down.visible = true
+	return new_hitbox
+
+func remove_hitbox():
+	var hitbox_name = hitbox_drop_down.get_item_text(hitbox_drop_down.selected)
+	var hitbox = state.find_child(hitbox_name)
+	
+	if hitbox_drop_down.item_count < 0:
+		hitbox_drop_down.visible = false
+	
+	for item in range(hitbox_drop_down.item_count):
+		if hitbox_drop_down.get_item_text(item) == state.name:
+			hitbox_drop_down.remove_item(item)
+	state.remove_child(hitbox)
+	hitbox.queue_free()
+
+# UI Reactions--------------------------------------------------------------------------------------
 func _on_state_drop_down_item_selected(index):
 	var current_state_name = state_drop_down.get_item_text(index)
 	state = state_machine.find_child(current_state_name)
@@ -54,44 +98,13 @@ func _on_state_drop_down_item_selected(index):
 	animation_player.play()
 	update_hitboxes(state.get_children())
 
-func update_hitboxes(hitboxes):
-	hitbox_drop_down.clear()
-	current_state_hitboxes = hitboxes
-	for hitbox in current_state_hitboxes:
-		hitbox_drop_down.add_item(hitbox.name)
-	if current_state_hitboxes.size() > 0: # dont select if there is no hitbox
-		_on_hitbox_drop_down_item_selected(hitbox_drop_down.selected)
-		hitbox_selection.show()
-	else:
-		hitbox_selection.hide()
+
 func _on_hitbox_drop_down_item_selected(index):
 	var new_hitbox = get_hitbox_from_name(hitbox_drop_down.get_item_text(index))
 	if new_hitbox:
 		hitbox_selection.update_hitbox(new_hitbox, character_root, state)
 	else:
 		push_error("Could not find hitbox")
-
-func get_hitbox_from_name(hitbox_name):
-	for hitbox in current_state_hitboxes:
-		if hitbox.name == hitbox_name:
-			return hitbox
-	return null
-
-func add_hitbox(state_node):
-	var new_hitbox = Area2D.new()
-	new_hitbox.set_script(load(hitbox_script_path))
-	
-	var collision = CollisionShape2D.new()
-	collision.shape = CircleShape2D.new()
-	collision.shape.radius = default_hitbox_radius
-	
-	state_node.add_child(collision)
-	
-	return new_hitbox
-
-func remove_hitbox(state_node:Node, hitbox):
-	state_node.remove_child(hitbox)
-	hitbox.queue_free()
 
 func _on_frame_slider_value_changed(value):
 	animation_player.stop()
@@ -107,3 +120,11 @@ func _on_frame_slider_drag_started():
 
 func _on_pause_pressed():
 	animation_player.pause()
+
+
+func _on_add_hitbox_pressed():
+	add_hitbox()
+
+
+func _on_remove_hitbox_pressed():
+	remove_hitbox()
