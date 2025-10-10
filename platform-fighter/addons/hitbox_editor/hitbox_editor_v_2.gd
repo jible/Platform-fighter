c9ae8377fbdbdee64eafc116d43e80bfe39cc059
jible <jamezman88th@gmail.com>
2025-10-10 03:38:39 -0700

    Hitbox Editor V2 progress
    - Set node references on script
    - Handled flow from opening tool, to populating hitbox ui
    - removed unused signals. In the next section connect the signals and paste the function contents back in as necessary
    -Still untested. Waiting to test until it is closer to working because it misfunctions may result in editing animations incorrectly.

--- platform-fighter/addons/hitbox_editor/hitbox_editor_v_2.gd
+++ platform-fighter/addons/hitbox_editor/hitbox_editor_v_2.gd
@@ -17,34 +17,41 @@ var animation: Animation
 
 
 # Cluster
+var clusters: Array[HitboxCluster] = []
 @export var cluster_drop_down: OptionButton
 @export var add_cluster: Button
 @export var remove_cluster: Button
 var cluster: HitboxCluster
-@export var turn_on_frame_field: SpinBox
-@export var turn_off_frame_field: SpinBox
+@export var cluster_turn_on_frame_field: SpinBox
+@export var cluster_turn_off_frame_field: SpinBox
 var hitboxes = []
 
 # Hitboxes
+@export var full_hitbox_ui_container: VBoxContainer
+@export var hitbox_turn_on_frame_field: SpinBox
+@export var hitbox_turn_off_frame_field: SpinBox
 @export var hitbox_drop_down: OptionButton
 @export var add_hitbox_button: Button
 @export var remove_hitbox_button: Button
 var hitbox: Hitbox
 
 @export var offset_info: AddRemoveField
-@export var turn_on_frame: SpinBox
-@export var turn_off_frame: SpinBox
 @export var damage_value: SpinBox
 @export var knockback_vector_x: SpinBox
 @export var knockback_vector_y: SpinBox
-@export var radius: SpinBox
+@export var height_selection: SpinBox
+@export var radius_selection: SpinBox
+@export var rotation_selection: SpinBox
 @export var knockback_magnitude: SpinBox
 
 
 
 # Animation
+@export var frame_slider: HSlider
 var sprite_manager_path: NodePath
 var hitbox_path: NodePath
+var cluster_path: NodePath
+
 
 var sprite_frame_track
 var hitbox_position_track
@@ -69,95 +76,103 @@ func configure(_character_root: BaseCharacter):
 
 func reconfigure():configure(base_character)
 
+# Updates cluster ui and selects first option
+func update_clusters():
+	cluster_drop_down.clear()
+	# As of right now, this will break if you name a cluster none
+	cluster_drop_down.add_item("None")
+	get_clusters()
+	for _cluster in clusters:
+		cluster_drop_down.add_item(_cluster.name)
+	# Not sure if this emits the signal or not
+	cluster_drop_down.select(0)
+	_on_cluster_drop_down_item_selected(cluster_drop_down.selected)
+
+
 # Populates hitbox ui. Call when new state is selected
-# Automatically Selects the first hitbox
-func update_hitboxes(_hitboxes):
+# Automatically selects the first hitbox
+func update_hitboxes():
+	get_hitboxes()
 	hitbox_drop_down.clear()
-	hitboxes = _hitboxes
-	for hitbox in hitboxes:
-		hitbox_drop_down.add_item(hitbox.name)
-	if hitboxes.size() > 0: # dont select if there is no hitbox
-		_on_hitbox_drop_down_item_selected(hitbox_drop_down.selected)
+	for _hitbox in hitboxes:
+		hitbox_drop_down.add_item(_hitbox.name)
+	_on_hitbox_drop_down_item_selected(hitbox_drop_down.selected)
 
+func get_hitboxes():
+	var parent = cluster if cluster else state
+	hitboxes = parent.find_children("", "Hitbox", false)
 
-	
+func get_clusters():
+	clusters = []
+	var clusters = state.find_children("", "HitboxCluster" , false)
+
 func set_new_hitbox(new_hitbox):
-	animation_library = animation_player.get_animation_library("")
-	animation = animation_library.get_animation(state.name)
-	
-	# update paths
-	var sprite_manager = base_character.sprite_manager
-	sprite_manager_path = animation_player.owner.get_path_to(sprite_manager)
-	
 	hitbox_path = animation_player.owner.get_path_to(hitbox)
 	# Update track references
-	get_track_references()
+	hitbox_method_track = get_track_reference( Animation.TrackType.TYPE_METHOD, hitbox_path.get_concatenated_names(), "visible")
+	hitbox_position_track = get_track_reference( Animation.TrackType.TYPE_VALUE, hitbox_path.get_concatenated_names(), "position")
+	hitbox_visibility_track = get_track_reference( Animation.TrackType.TYPE_VALUE, hitbox_path.get_concatenated_names(), "position")
 	# If there is a null track reference, create it 
-	auto_fill_tracks()
+	if hitbox_position_track == null:
+		hitbox_position_track = create_track( Animation.TYPE_VALUE, hitbox_path, "position", hitbox.position)
+	if hitbox_visibility_track == null:
+		hitbox_visibility_track = create_track( Animation.TYPE_VALUE, hitbox_path, "visible")
+	if hitbox_method_track == null:
+		hitbox_method_track = create_track( Animation.TYPE_METHOD, hitbox_path)
 	
-	frame_time = animation.track_get_key_time(sprite_frame_track, 1)
-	
+	var turn_on_off_fields = [
+		hitbox_turn_on_frame_field,
+		hitbox_turn_off_frame_field
+	]
+	for i in turn_on_off_fields:
+		i.visibility = (cluster == null)
 	# Extracts keys from anims and populates ui with their info
 	set_hitbox_position_keys()
-	set_hitbox_method_keys()
+	set_hitbox_toggle_keys()
 	
-	secret_set(damage_value, hitbox.damage)
-	secret_set(knockback_vector_x, hitbox.knockback_vector.x)
-	secret_set(knockback_vector_y, hitbox.knockback_vector.y)
-	secret_set(radius, hitbox.collision_shape.shape.radius)
-	secret_set(knockback_magnitude, hitbox.knockback_magnitude)
+	damage_value.set_value_no_signal(hitbox.damage)
+	knockback_vector_x.set_value_no_signal( hitbox.knockback_vector.x)
+	knockback_vector_y.set_value_no_signal(hitbox.knockback_vector.y)
+	knockback_magnitude.set_value_no_signal(hitbox.knockback_magnitude)
+	radius_selection.set_value_no_signal(hitbox.collision_shape.shape.radius)
+	height_selection.set_value_no_signal(hitbox.collision_shape.shape.height)
+	rotation_selection.set_value_no_signal(hitbox.rotation_degrees)
+	
+
+# Helper function for making tracks and optionally adding a default key
+# Does not automatically populate with key if default value is set to null
+func create_track( anim_type, node_path, attribute = null, default_value = null, interpolation_type = Animation.InterpolationType.INTERPOLATION_NEAREST, update_mode = Animation.UpdateMode.UPDATE_DISCRETE):
+	var new_track = animation.add_track(Animation.TYPE_VALUE)
+	var track_path = NodePath(String(node_path) +  (":" +attribute) if attribute else "" )
+	animation.track_set_path(new_track,  track_path)
+	if default_value:
+		animation.track_insert_key(new_track,0,default_value)
+	if anim_type != Animation.TYPE_METHOD:
+		animation.track_set_interpolation_type(new_track, interpolation_type)
+		animation.value_track_set_update_mode(new_track, update_mode)
+	return new_track
 
 func secret_set(obj, value):
 	obj.set_block_signals(true)
 	obj.value = value
 	obj.set_block_signals(false)
 
-func get_track_references():
-	hitbox_position_track = null
-	sprite_frame_track = null
-	hitbox_method_track = null
-	hitbox_visibility_track = null
-	
+
+
+
+# Slightly expensive aproach to extracting paths, but the code is so much cleaner to use
+func get_track_reference(target_track_type, target_track_path, target_track_sub_name):
 	for track in range(animation.get_track_count()):
 		var track_type = animation.track_get_type(track)
 		var path = animation.track_get_path(track)
+		if track_type == target_track_type:
+			if path.get_concatenated_names() == target_track_path \
+			and path.get_concatenated_subnames() == target_track_sub_name: 
+				return track
+	return null
 
-		if track_type == Animation.TrackType.TYPE_VALUE:
-			if path.get_concatenated_names() == hitbox_path.get_concatenated_names() \
-			and path.get_concatenated_subnames() == "position": 
-				hitbox_position_track = track
-			elif  path.get_concatenated_names() == sprite_manager_path.get_concatenated_names() \
-			and path.get_concatenated_subnames() == "frame": 
-				sprite_frame_track = track
-			elif path.get_concatenated_names() == hitbox_path.get_concatenated_names() \
-			and path.get_concatenated_subnames() == "visible":
-				hitbox_visibility_track = track 
-		elif track_type == Animation.TrackType.TYPE_METHOD:
-			if path.get_concatenated_names() == hitbox_path.get_concatenated_names(): 
-				hitbox_method_track = track
 # Animation Extraction and Sync---------------------------------------------------------------------
-func auto_fill_tracks():
-	if hitbox_position_track == null:
-		hitbox_position_track = animation.add_track(Animation.TYPE_VALUE)
-		animation.track_set_path(hitbox_position_track, NodePath(String(hitbox_path) + ":position") )
-		var default_key = animation.track_insert_key(hitbox_position_track,0,hitbox.position)
-		_set_track_defaults(hitbox_position_track)
-	if sprite_frame_track == null:
-		# TODO Add default key
-		sprite_frame_track =animation.add_track(Animation.TYPE_VALUE)
-		animation.track_set_path(sprite_frame_track, NodePath(String(sprite_manager_path) + ":frame") )
-		_set_track_defaults(sprite_frame_track)
-	if hitbox_visibility_track == null:
-		hitbox_visibility_track = animation.add_track(Animation.TYPE_VALUE)
-		animation.track_set_path(hitbox_visibility_track, NodePath(String(hitbox_path) + ":visible"))
-		_set_track_defaults(hitbox_visibility_track)
-		var default_key = animation.track_insert_key(hitbox_visibility_track, 0,false)
-	if hitbox_method_track == null:
-		# TODO Add default key
-		hitbox_method_track =animation.add_track(Animation.TYPE_METHOD)
-		animation.track_set_path(hitbox_method_track, hitbox_path)
-		
-
+# Updates ui to match hitbox pos animation
 func set_hitbox_position_keys():
 	offset_info.reset()
 	for key in range(animation.track_get_key_count(hitbox_position_track)):
@@ -168,54 +183,51 @@ func set_hitbox_position_keys():
 		# add keys to ui
 		var new_field: PositionAnimationSetter = offset_info.internal_add_field()
 		new_field.silent_populate(int(frame), hitbox_position.x, hitbox_position.y)
-		new_field.frame_changed.connect(on_frame_changed.bind(new_field))
-		new_field.pos_changed.connect(on_position_changed.bind(new_field))
+		new_field.frame_changed.connect(on_hitbox_offset_frame_changed.bind(new_field))
+		new_field.pos_changed.connect(on_hitbox_offset_position_changed.bind(new_field))
 
 
 
 
-
-func set_hitbox_method_keys():
-	var has_turn_off = false
-	var has_turn_on = false
-	for key in range(animation.track_get_key_count(hitbox_method_track)):
-		if animation.method_track_get_name(hitbox_method_track,key) == "turn_on":
-			has_turn_on = true
-			var time = animation.track_get_key_time(hitbox_method_track, key)
-			var frame = time_to_frame(time)
-			turn_on_frame.set_block_signals(true)
-			turn_on_frame.value = frame
-			old_turn_on_frame = frame
-			turn_on_frame.set_block_signals(false)
-	
-	
-		elif animation.method_track_get_name(hitbox_method_track,key) == "turn_off":
-			has_turn_off = true
-			var time = animation.track_get_key_time(hitbox_method_track, key)
-			var frame = time_to_frame(time)
-			turn_off_frame.set_block_signals(true)
-			turn_off_frame.value = frame
-			old_turn_off_frame = frame
-			turn_off_frame.set_block_signals(false)
-			
-	if !has_turn_on:
-		animation.track_insert_key(hitbox_method_track, 0, {
-			"method" : "turn_on",
-			"args":[],
-		})
-		turn_on_frame.value = animation.track_get_key_time(hitbox_method_track, 0)
-		
-	if !has_turn_off:
-		animation.track_insert_key(hitbox_method_track, frame_to_time(1), {
-			"method" : "turn_off",
-			"args":[],
-		})
-		turn_off_frame.value = animation.track_get_key_time(hitbox_method_track, 1)
-		
-	
+func set_hitbox_toggle_keys():
+	var method_frames = extract_and_correct_on_off_time(hitbox_method_track)
+	hitbox_turn_on_frame_field.value = method_frames["turn_on"]
+	hitbox_turn_off_frame_field.value = method_frames["turn_off"]
 	
 	sync_visibility_track()
 
+
+# Automatically parses given track for keys that call turn on and turn off
+# If more or less than 1 key for either function, corrects it. Then returns the method frames
+func extract_and_correct_on_off_time(track):
+	var method_frames = {
+		"turn_on": null,
+		"turn_off" : null,
+	}
+	var default_key_frame = 0
+	for key in range(animation.track_get_key_count(track) -1, -1, -1):
+		var time = animation.track_get_key_time(track, key)
+		var frame = time_to_frame(time)
+		var method_name =  animation.method_track_get_name(track,key)
+		if method_name == "turn_on" or method_name == "turn_off":
+			if method_frames[method_name] != null:
+				animation.track_remove_key(track,key)
+				continue
+			method_frames[method_name] = frame
+			if default_key_frame == frame: default_key_frame += 1
+	
+	for key in method_frames.keys():
+		if method_frames[key] == null:
+			animation.track_insert_key(track, frame_to_time(default_key_frame), {
+				"method" : key,
+				"args":[],
+			})
+			method_frames[key] = default_key_frame
+			default_key_frame += 1
+	return method_frames
+
+
+# Makes hitbox visibility track match that of the on and off keys of the method track (this allows hitbox toggling visualization in editor.
 func sync_visibility_track():
 	# Clear Visibility track
 	for key in range(animation.track_get_key_count(hitbox_visibility_track)):
@@ -223,21 +235,13 @@ func sync_visibility_track():
 	# Populate track with keys from method track
 	for key in range(animation.track_get_key_count(hitbox_method_track)):
 		var time = animation.track_get_key_time(hitbox_method_track, key)
-		var frame = time_to_frame(time)
 		match animation.method_track_get_name(hitbox_method_track,key):
 			"turn_on":
 				animation.track_insert_key(hitbox_visibility_track, time, true)
 			"turn_off":
 				animation.track_insert_key(hitbox_visibility_track, time, false)
 
-
-
-func _set_track_defaults(track):
-	animation.track_set_interpolation_type(track, Animation.InterpolationType.INTERPOLATION_NEAREST)
-	animation.value_track_set_update_mode(track, Animation.UpdateMode.UPDATE_DISCRETE)
-
 # Animation Editing Helpers-------------------------------------------------------------------------
-
 func time_to_frame(time):
 	return round(time/frame_time)
 	
@@ -250,9 +254,9 @@ func is_valid_frame(frame):
 		if current_key_frame == frame:
 			return false
 	return true
-# UI Signal Handlers--------------------------------------------------------------------------------
 
-func on_frame_changed(position_animation:PositionAnimationSetter):
+# UI Signal Handlers--------------------------------------------------------------------------------
+func on_hitbox_offset_frame_changed(position_animation:PositionAnimationSetter):
 
 	var new_frame = position_animation.frame_field.value
 	var old_frame = position_animation.old_frame
@@ -266,75 +270,7 @@ func on_frame_changed(position_animation:PositionAnimationSetter):
 	else:
 		position_animation.silent_set_frame(old_frame)
 
-func _on_pos_anim_value_changed(former_frame, frame, pos):
-	var key = animation.track_find_key(hitbox_position_track,frame_to_time(former_frame),Animation.FIND_MODE_NEAREST)
-	animation.track_set_key_time(hitbox_position_track , key, frame_to_time(frame))
-
-
-func _on_add_remove_field_field_added(position_animation: PositionAnimationSetter):
-	var min_frame = 0
-	for key in range(animation.track_get_key_count(hitbox_position_track) ):
-		var current_key_frame = time_to_frame(animation.track_get_key_time(hitbox_position_track, key))
-		if  current_key_frame == min_frame:
-			min_frame += 1
-		else: break
-	position_animation.silent_set_frame(min_frame)
-	var new_key = animation.track_insert_key(hitbox_position_track, frame_to_time(min_frame), Vector2(0,0))
-	
-	# Connect signals
-	position_animation.frame_changed.connect(on_frame_changed.bind(position_animation))
-	position_animation.pos_changed.connect(on_position_changed.bind(position_animation))
-
-
-func _on_add_remove_field_field_removed(position_animation :PositionAnimationSetter):
-	var frame = position_animation.frame_field.value
-	var key = animation.track_find_key(hitbox_position_track, frame_to_time(frame))
-	animation.track_remove_key(hitbox_position_track, key)
-
-var old_turn_on_frame = null
-
-func _on_turn_on_frame_field_value_changed(value):
-	var turn_on_key
-	for key in range(animation.track_get_key_count(hitbox_method_track)):
-		var method_name = animation.method_track_get_name(hitbox_method_track, key)
-		if method_name == "turn_on":
-			turn_on_key = key
-		elif method_name == "turn_off":
-			if value == time_to_frame(animation.track_get_key_time(hitbox_method_track, key)):
-				turn_on_frame.set_block_signals(true)
-				turn_on_frame.value = old_turn_on_frame
-				turn_on_frame.set_block_signals(false)
-				return
-	animation.track_set_key_time(hitbox_method_track, turn_on_key, frame_to_time(value))
-	animation_player.seek(frame_to_time(value))
-	animation_player.advance(0)
-	animation_player.pause()
-	old_turn_on_frame = value
-	sync_visibility_track()
-
-var old_turn_off_frame = null
-
-func _on_turn_off_frame_field_value_changed(value):
-	var turn_off_key
-	
-	for key in range(animation.track_get_key_count(hitbox_method_track)):
-		var method_name = animation.method_track_get_name(hitbox_method_track, key)
-		if method_name == "turn_off":
-			turn_off_key = key
-		elif method_name == "turn_on":
-			if value == time_to_frame(animation.track_get_key_time(hitbox_method_track, key)):
-				turn_off_frame.set_block_signals(true)
-				turn_off_frame.value = old_turn_off_frame
-				turn_off_frame.set_block_signals(false)
-				return
-	animation.track_set_key_time(hitbox_method_track, turn_off_key, frame_to_time(value))
-	old_turn_off_frame = value
-	animation_player.seek(frame_to_time(value))
-	animation_player.advance(0)
-	animation_player.pause()
-	sync_visibility_track()
-
-func on_position_changed(position_animation:PositionAnimationSetter):
+func on_hitbox_offset_position_changed(position_animation:PositionAnimationSetter):
 	print('changed')
 	var new_position = Vector2(position_animation.x_field.value, position_animation.y_field.value)
 	var frame = position_animation.frame_field.value
@@ -345,15 +281,53 @@ func on_position_changed(position_animation:PositionAnimationSetter):
 	animation_player.pause()
 
 
-
 # Signal Handlers ----------------------------------------------------------------------------------
 func _on_state_drop_down_item_selected(index):
-	pass # Replace with function body.
+	var current_state_name = state_drop_down.get_item_text(index)
+	state = state_machine.find_child(current_state_name)
+	if !state:
+		print("state does not exist")
+		return
+		
+	animation_library = animation_player.get_animation_library("")
+	animation = animation_library.get_animation(state.name)
+	sprite_frame_track = get_track_reference( Animation.TrackType.TYPE_VALUE, sprite_manager_path.get_concatenated_names(), "frame")
+	if !sprite_frame_track:
+		print("SpriteFrame does not have anim track. Use animation updater to add the anim track then click 'Refresh'")
+		return
+	var sprite_manager = base_character.sprite_manager
+	sprite_manager_path = animation_player.owner.get_path_to(sprite_manager)
+	
+	frame_time = animation.track_get_key_time(sprite_frame_track, 1)
+	
+	frame_slider.max_value = animation_player.get_animation(state.name).length
+	animation_player.current_animation = state.name
+	animation_player.seek(0,true)
+	animation_player.pause()
+	update_clusters()
 
 func _on_refresh_button_pressed(): reconfigure()
 
 func _on_hitbox_drop_down_item_selected(index):
+	if index == -1:
+		full_hitbox_ui_container.hide()
+		return
+	full_hitbox_ui_container.show()
 	var new_hitbox = state.find_child(hitbox_drop_down.get_item_text(index))
 	if !new_hitbox:
 		push_error("Could not find hitbox")
 		set_new_hitbox(new_hitbox)
+
+
+func _on_cluster_drop_down_item_selected(index):
+	var selected_name = cluster_drop_down.get_item_text(index)
+	cluster = null
+	if selected_name != "none":
+		cluster = state.find_child(selected_name, false)
+		cluster_path = animation_player.owner.get_path_to(cluster)
+	cluster_turn_off_frame_field.visible = (cluster != null)
+	cluster_turn_on_frame_field.visible = (cluster != null)
+	
+	#TODO Get node paths and anim tracks for all of the hitboxes that belong to this cluster
+	
+	update_hitboxes()
