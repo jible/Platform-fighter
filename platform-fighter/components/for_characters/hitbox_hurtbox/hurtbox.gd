7390e62926b19b850c0a4cdc7ed1c19b2cbe91a7
jible <jamezman88th@gmail.com>
2025-11-01 13:21:04 -0700

    hitbox mask and hurtbox layer fixed.

--- platform-fighter/components/for_characters/hitbox_hurtbox/hurtbox.gd
+++ platform-fighter/components/for_characters/hitbox_hurtbox/hurtbox.gd
@@ -10,8 +10,9 @@ var health = null
 func configure():
 	if !base_character: 
 		return
-	collision_layer = 0
-	collision_layer += 1 << base_character.team_number
+	collision_layer = 1 << (base_character.team_number + GlobalResources.team_one_collision_bit)
+	collision_mask = 0
+
 
 	health = base_character.health
 
