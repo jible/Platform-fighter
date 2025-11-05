9a6c404fc2ab928cdc2c56198395726cc5ebf48d
jible <jamezman88th@gmail.com>
2025-11-04 23:48:36 -0800

    fixed rendering bug for rectangles and capsules

--- platform-fighter/addons/deterministic_physics/shapes/dp_capsule.gd
+++ platform-fighter/addons/deterministic_physics/shapes/dp_capsule.gd
@@ -2,5 +2,5 @@
 extends DP_Collision_Shape
 class_name DP_Capsule
 
-var radius: DM_Decimal
-var height: DM_Decimal
+var radius: DM_Decimal = DM_Decimal.from_int(50)
+var height: DM_Decimal= DM_Decimal.from_int(50)
--- platform-fighter/addons/deterministic_physics/shapes/dp_rectangle.gd
+++ platform-fighter/addons/deterministic_physics/shapes/dp_rectangle.gd
@@ -3,5 +3,5 @@ extends DP_Collision_Shape
 class_name DP_Rectangle
 
 
-var height:DM_Decimal = DM_Decimal.from_int(0)
-var width:DM_Decimal = DM_Decimal.from_int(0)
+var height:DM_Decimal = DM_Decimal.from_int(50)
+var width:DM_Decimal = DM_Decimal.from_int(50)
