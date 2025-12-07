86994c9bcf2a41de6467648391a0577bd3913645
jible <jamezman88th@gmail.com>
2025-12-07 13:51:34 -0800

    started testing circle overlap. Not quite working yet

--- platform-fighter/scenes/testing_scenes/dummy_test_scripts/ShapeRenderTest.cs
+++ platform-fighter/scenes/testing_scenes/dummy_test_scripts/ShapeRenderTest.cs
@@ -5,10 +5,26 @@ using System.Numerics;
 public partial class ShapeRenderTest : Node3D
 {
     [Export] public dp_object moveable;
+
+    public override void _Ready()
+    {
+        
+        moveable.ObjectEntered += overlapped;
+    }
+
+    private void overlapped(dp_object other)
+    {
+        GD.Print("entered");
+
+    }
+
+
     public override void _Process(double delta)
     {
         var inputDir = Input.GetVector("debug_left", "debug_right", "debug_down", "debug_up");
         moveable.Shape.Position += new DM_Vector2((inputDir/ 7));
     }
+
+ 
 
 }
