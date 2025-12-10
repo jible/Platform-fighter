c300dcbdf5e0a282c163d3d3e394821fb82e486b
jible <jamezman88th@gmail.com>
2025-12-10 13:35:24 -0800

    fixed small rendering bug. Player now renders correctly with collision bounces

--- platform-fighter/addons/cpp_overhall/deterministic_physics/dp_shape_follower.cs
+++ platform-fighter/addons/cpp_overhall/deterministic_physics/dp_shape_follower.cs
@@ -5,20 +5,20 @@ using System;
 [Tool]
 public partial class dp_shape_follower : Node3D
 {
-    [Export] dp_object Target;
-    [Export] float ZIndex = 0f;
-    public override void _Process(double delta)
-    {
-        if (Target == null) return;
-        dp_shape shape = Target.Shape;
-        if (shape == null) return;
-        if (Engine.IsEditorHint()) Position = VectorUp(shape.EditorPosition, ZIndex);
-        else Position = VectorUp(shape.Position.ToStandardVector(), ZIndex);
-    }
+	[Export] dp_object Target;
+	[Export] float ZIndex = 0f;
+	public override void _Process(double delta)
+	{
+		if (Target == null) return;
+		dp_shape shape = Target.Shape;
+		if (shape == null) return;
+		if (Engine.IsEditorHint()) Position = VectorUp(shape.EditorPosition, ZIndex);
+		else Position = VectorUp(shape.Position.ToStandardVector(), ZIndex);
+	}
 
-    private Vector3 VectorUp(Vector2 v, float z)
-    {
-        return new (v.X, v.Y, z);
-    }
+	private Vector3 VectorUp(Vector2 v, float z)
+	{
+		return new (v.X, v.Y, z);
+	}
 
 }
