a5f2de33466c39ae42fafe4596031055175fc176
jible <jamezman88th@gmail.com>
2025-12-21 11:30:44 -0800

    Revert "Entirely broken"
    This reverts commit 0f61d7a53b373559abad5a6a1c29cd68afade24a.

--- platform-fighter/addons/deterministic_physics/deterministic_physics.gd
+++ platform-fighter/addons/deterministic_physics/deterministic_physics.gd
@@ -16,37 +16,21 @@ func _disable_plugin():
 
 
 func _enter_tree():
-	scene_root = get_tree().edited_scene_root if Engine.is_editor_hint() else get_tree().current_scene
+	scene_root = get_tree().current_scene
 	_on_scene_changed(scene_root)
 	scene_changed.connect(_on_scene_changed)
 
 func _exit_tree():
-	pass
-	#root.remove_child(physics_server)
-	#physics_server.queue_free()
-	#root.remove_child(physics_renderer)
-	#physics_renderer.queue_free()
+	remove_child(physics_server)
+	physics_server.queue_free()
+	remove_child(physics_renderer)
+	physics_renderer.queue_free()
 
 func _on_scene_changed(root: Node):
-	#clean_up_managers()
+	print(root)
 	if (! root): return
 	physics_server = dp_physics_server.new()
 	physics_renderer= dp_shape_renderer_3d.new()
 	
 	root.add_child(physics_server)
 	root.add_child(physics_renderer)
-	
-func clean_up_managers():
-	if (physics_renderer and physics_renderer.is_valid_instance()):
-		var parent = physics_renderer.get_parent()
-		if (parent):
-			parent.remove_child(physics_renderer)
-			physics_renderer.queue_free()
-			physics_renderer = null
-	
-	if (physics_server and physics_server.is_valid_instance()):
-		var parent = physics_server.get_parent()
-		if (parent):
-			parent.remove_child(physics_server)
-			physics_server.queue_free()
-			physics_server = null
--- platform-fighter/addons/deterministic_physics/physics_objects/dp_shape_renderer_3d.cs
+++ platform-fighter/addons/deterministic_physics/physics_objects/dp_shape_renderer_3d.cs
@@ -21,11 +21,6 @@ public partial class dp_shape_renderer_3d : Node3D
     {
         GlobalInstance = this;
     } 
-
-    public static dp_shape_renderer_3d GetGlobalInstance()
-    {
-        return GlobalInstance;
-    }
     public override void _PhysicsProcess(double delta)
     {
         
@@ -39,8 +34,8 @@ public partial class dp_shape_renderer_3d : Node3D
         
         UpdateFollowerPositions();
         if (!Engine.IsEditorHint() && !RenderShapesInPlay) return;
-        
-        Node CurrentScene = Engine.IsEditorHint() ? GetTree().EditedSceneRoot: GetTree().CurrentScene;
+
+        Node CurrentScene = (Engine.IsEditorHint()) ? GetTree().EditedSceneRoot: GetTree().CurrentScene;
 
         var seenShapes = new HashSet<dp_object>();
 
@@ -168,6 +163,7 @@ public partial class dp_shape_renderer_3d : Node3D
         }
 
         meshInstance.Position = ObjPos;
+        // e
         // Rectangle Data:
         // [shape, color, size]
         RenderState ObjPrevData;
