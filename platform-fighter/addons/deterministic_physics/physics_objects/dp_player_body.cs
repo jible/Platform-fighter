304a5756bf195e5953e5ab9b24b899b0b658d8b2
jible <jamezman88th@gmail.com>
2026-01-01 13:56:39 -0800

    Deleting almost everything and cleaning things up for the plugin

--- platform-fighter/addons/deterministic_physics/physics_objects/dp_player_body.cs
+++ platform-fighter/addons/deterministic_physics/physics_objects/dp_player_body.cs
@@ -8,17 +8,17 @@ public partial class dp_player_body : dp_object
 {
 	public DM_Vector2 Velocity = new();
 	
-    [Export] float EditorAcceleration = new();
-    DM64 Acceleration;
+	[Export] float EditorAcceleration = new();
+	DM64 Acceleration;
 
-    [Export] float EditorMaxVelocity = new();
-    DM64 MaxVelocity = new();
+	[Export] float EditorMaxVelocity = new();
+	DM64 MaxVelocity = new();
 
-    public void Tick()
-    {
-        Velocity += Acceleration * new DM_Vector2(1,1);
+	public void Tick()
+	{
+		Velocity += Acceleration * new DM_Vector2(1,1);
 		Position += Velocity;
-    } 
+	} 
 
 	// public bool IsGrounded()
 	// {
--- platform-fighter/components/for_scenes/character_spawn_package.gd
+++
@@ -1,7 +0,0 @@
-class_name CharacterSpawnPackage
-extends Resource
-
-
-@export var character_profile: CharacterProfile
-@export var player_number: int = 0
-@export var team_number: int = 0
