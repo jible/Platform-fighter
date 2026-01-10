188db7c824779836812fc08c2bf0c76681edac0c
jible <jamezman88th@gmail.com>
2026-01-10 12:31:44 -0800

    Played with test character scale and tested moving player via velocity property.

--- platform-fighter/addons/deterministic_physics/physics_objects/dp_player_body.cs
+++ platform-fighter/addons/deterministic_physics/physics_objects/dp_player_body.cs
@@ -4,18 +4,19 @@ using System.ComponentModel;
 
 [GlobalClass]
 [Tool]
-public partial class dp_player_body : dp_object
+public partial class dp_player_body : dp_object, ITickable
 {
 	public DM_Vector2 Velocity = new();
 	
 	[Export] float EditorAcceleration = new();
-	DM64 Acceleration = new();
+	DM64 Acceleration = new(.0025f);
 
 	[Export] float EditorMaxVelocity = new();
 	DM64 MaxVelocity = new();
 
 	public void Tick()
 	{
+		GD.Print("ticking");
 		Velocity += Acceleration * new DM_Vector2(1,1);
 		Position += Velocity;
 	} 
--- platform-fighter/components/scene/CharacterHolder.cs
+++ platform-fighter/components/scene/CharacterHolder.cs
@@ -6,7 +6,6 @@ public partial class CharacterHolder : Node, ITickable
 {
     public PlayerManager playerManager;
     [Export] public PlayManager playManager; 
-    List<Node> Players= [];
 
     public void Config()
     {
@@ -23,14 +22,16 @@ public partial class CharacterHolder : Node, ITickable
     public void InstancePlayers()
     {
         foreach(var Player in playerManager.AllPlayers){
-            if (Player == null){ continue;}
+            if (Player== null) continue;
 
-
+            
             var characterPath = Player.SelectedCharacterPath;
             PackedScene scene = (PackedScene)GD.Load(characterPath);
             BaseCharacter3d instance = (BaseCharacter3d)scene.Instantiate();
 
             AddChild(instance);
+
+            Player.Instance= instance;
             instance.Name = "Player" + Player.PlayerNumber.ToString();
             instance.ConfigurePlayer(Player.TeamNumber, Player.PlayerNumber, Player.playerTag);
         }
