98389e1cc8266723c5b92663a61321835f2178e6
jible <jamezman88th@gmail.com>
2026-01-23 13:22:39 -0800

    Send packet method should be functioning
    This is untested! It should work for both client and host. However the receiver just emits a signal of the encoded bytes, so each node that sends data through the rollback manager needs to establish its own listening to the signal and how it decodes/ handles the signal.

--- platform-fighter/components/Globals/RollbackManager.cs
+++ platform-fighter/components/Globals/RollbackManager.cs
@@ -1,6 +1,9 @@
 using Godot;
+using Godot.NativeInterop;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public partial class RollbackManager : Node
@@ -9,7 +12,7 @@ public partial class RollbackManager : Node
     public static int MAX_ROLLBACK_FRAMES = 50;
 
 
-    enum PacketType
+    public enum PacketType
     {
         NOTIFY_HOST_OF_CONNECTION,
         POLL_ROUND_TRIP_TICKS,
@@ -19,18 +22,22 @@ public partial class RollbackManager : Node
     }
 
     
-    NetworkManager.ConnectionType connectionType;
-
+    [Signal] // The packet type is an int since it doesn't like when you put non-godot types (like c# enums) in signals
+    public delegate void UdpDataReceivedEventHandler(int packetType, byte[] Packet);    
 
     PacketPeerUdp udp;
     int RoundTripTicks = 0;
-    UserNetworkData HostData;
-    List<UserNetworkData> ClientData= [];
+    HashSet<Tuple<string, int>> ClientData= new();
     List<object> ClientLatency = [];
 
     public override void _EnterTree()
     {
         GloablInstance = this;
+    }
+
+    public Tuple<string, int> MakeUserID( string Address, int Port)
+    {
+        return new (Address, Port);
     }
 
     private void StartHostRollback(int Port, String IpAddress)
@@ -41,20 +48,83 @@ public partial class RollbackManager : Node
         {
             throw new ArgumentException($"Failed to bind UDP on port {Port}");
         }
-        HostData = new( IpAddress, Port);
     }
 
-    public void PrepareToJoinRollback(string Address, int Port)
+    public void NotifyHostOfConnection()
+    {
+        byte[] Packet = [(byte)PacketType.NOTIFY_HOST_OF_CONNECTION];
+        udp.PutPacket(Packet);
+    }
+
+    public void JoinRollback(string ClientAddress, int ClientPort, string HostAddress, int HostPort)
     {
         udp = new PacketPeerUdp();
-        var err = udp.Bind(0);
+        var err = udp.Bind(ClientPort, ClientAddress);
         if (err != Error.Ok)
         {
             throw new ArgumentException("Failed to bind UDP  client socket");
         }
+        udp.ConnectToHost(HostAddress, HostPort);
 
     }
 
+    public void SendEncodedPackets(PacketType packetType, byte[] EncodedPacket)
+    {
+        byte[] Buffer =[];
+        Buffer.Concat([(byte)packetType]);
+        Buffer.Concat(EncodedPacket);
+
+        udp.PutPacket(Buffer);
+
+    }
+
+    public void HandlePacket(byte[] Packet, string Address, int Port)
+    {
+        if (Packet.Count() < 1)
+        {
+            // Dropped bytes
+            return;
+        }
+
+        PacketType packetType = (PacketType)Packet[0];
+        byte[] PacketWithNoPrefix = Packet[1..];
+        
+        EmitSignal("UdpDataReceived", (int)packetType, PacketWithNoPrefix);
+
+        // switch case for handling message the rollback manager will use (like latency polling)
+        switch(packetType){
+            case PacketType.NOTIFY_HOST_OF_CONNECTION:
+                ClientData.Add(MakeUserID(Address, Port));
+                break;
+        }
+    }
+
+
+    public void HandleInputPacket(byte[] Packet)
+    {
+        
+    }
+
+
+    public override void _Process(double delta)
+    {
+        if (NetworkManager.GlobalInstance.connectionType == NetworkManager.ConnectionType.DISCONNECTED)
+        {
+            return;
+        }
+
+        if ( udp != null && udp.GetAvailablePacketCount() != 0)
+        {
+         var Packet = udp.GetPacket();
+            
+            string Address = udp.GetPacketIP();
+            int Port = udp.GetPacketPort();
+            HandlePacket(Packet, Address,Port);
+            
+        }
+
+
+    }
 
 }
 
