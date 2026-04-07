a1b8144423e89a67f75c6486a1ebf352a9fe6907
jible <jamezman88th@gmail.com>
2026-04-07 14:06:26 -0700

    fixed some build only mac errors by
    Fixed build error on state machine and  planned state machine structure in state_machine.cs
    
    Mac still has build issue where .uid files have different capitalization from .cs files. However they cannot be pushed because mac doesn't recognize the change ( I think).

--- platform-fighter/components/character/state_machine/state_machine.cs
+++ platform-fighter/components/character/state_machine/state_machine.cs
@@ -6,9 +6,8 @@ using Godot;
 public partial class CharacterStateMachine<StateEnum>:Node where StateEnum : Enum
 {
     public Dictionary<StateEnum, State<StateEnum>> StateToObject;
-    public State<StateEnum>? CurrentState = null;
-    // Some State make sense 
-    // If you need a collection of 
+    public State<StateEnum> CurrentState = null;
+
     public enum StateBucketKeys {
         Attack
     }
@@ -56,6 +55,8 @@ public abstract class State<StateEnum> where StateEnum: Enum
     public abstract void Tick();
 }
 
+
+// Sample state machine 
 public partial class JohnnyStateMachine : CharacterStateMachine<JohnnyStateMachine.States>
 {
     public enum States {};
@@ -66,3 +67,54 @@ public partial class JohnnyStateMachine : CharacterStateMachine<JohnnyStateMachi
 
 
 }
+
+
+/*
+Imagined workflow:
+
+
+Making a new character
+you manually add a new state machine? (This is a little annoying since it doesn't inherit the references)
+
+
+
+Ideally: You can add a new character and their default input flow already 
+works for the state machine and you don't need to set references. You just 
+have to attatch animation and set properties like movement attributes.
+
+You won't need to change anything about the statemachine, just the states, which have exportable properties for everything.
+- Acceleration
+- Max Velocity
+- Animation
+
+- Apply color?
+- Particles?
+
+
+How is this achieveable?
+
+Base character state machine already comes with all of the states.
+Somehow, you can add new methods to each state?
+
+- What methods would you need?
+    - instancing projectiles? Can this be done in the animation?
+    - recoil damage? <- This can be done in the animation with generalized method
+    - Set (super/heavy) armor? <- this can be done in the animation
+
+
+
+
+If states are nodes, you can put export properties on them! This makes nodes a good candidate for states.
+you would export animation references( or just use naming conventions)?
+
+How do states respond to inputs? 
+Signal based? 
+
+
+Got it:
+
+States are nodes with exportable properties: including their method resoucres
+They default to a reference with no methods, however you can make a new one 
+'and replace that resource with a different version that has the mthods you need!
+
+*/
